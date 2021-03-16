da7ba13a5eeb4dca814c915103c035e9a216c94a
Vander <vlvsjr@gmail.com>
2021-03-16 10:12:43 -0300

    Implement solution to 'Character Input' exercise

---
+++ 1_character_input.py
@@ -0,0 +1,20 @@
+'''
+Create a program that asks the user to enter their name and their age.
+Print out a message addressed to them that tells them the year that 
+they will turn 100 years old.
+
+Extra: Ask the user for another number and print out that many copies 
+of the previous message.
+'''
+
+from datetime import datetime
+
+user_name = input("What's your name?\n")
+user_age = int(input("\nHow many years of age do you complete this year?\n"))
+number_of_copies = int(input("\nInput a random number:\n"))
+
+current_year = datetime.now().year
+hundredth_year = current_year - user_age + 100
+
+message = f"\n{user_name}, you're gonna be 100 years old in {hundredth_year}."
+print(number_of_copies * message)
