ceaad62f55792167baa7200e17cb1b1d511c5a27
Vander <vlvsjr@gmail.com>
2021-03-16 11:28:38 -0300

    Fix wording on 'List Less Than Ten' description

--- 3_list_less_than_ten.py
+++ 3_list_less_than_ten.py
@@ -1,11 +1,11 @@
 '''
 Take a list of numbers and write a program that prints out all the 
-elements of the list that are less-than 5.
+elements of the list that are less-than 10.
 
 Extras:
 1. Instead of printing the elements one by one, make a new list that 
-has all the elements less-than 5 from the original list in it and print 
-out this new list;
+has all the elements less-than 10 from the original list in it and 
+print out this new list;
 2. Ask the user for a number and return a list that contains only 
 elements from the original list that are smaller than the number given 
 by the user.
