3a005cc35a0479e0ecbf8158fa403973c1779170
Vander <vlvsjr@gmail.com>
2021-03-17 15:01:56 -0300

    Solve 'Check Primality Functions' exercise

---
+++ 11_check_primality_functions.py
@@ -0,0 +1,29 @@
+'''
+Ask the user for a number and determine whether the number is prime or 
+not. Create functions for the solution.
+'''
+
+def get_user_input():
+    global input
+    input = int(input("Input an integer to check its primality: "))
+
+def primality_of(number) -> bool:
+    if number <= 3:
+        return number > 1
+    if number % 2 == 0 or number % 3 == 0:
+        return False
+    i = 5
+    while i ** 2 <= number:
+        if number % i == 0 or number % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def print_information_on_input_primality():
+    if primality_of(input) is True:
+        print(f"{input} is a prime number!")
+    else:
+        print(f"{input} is not a prime number.")
+
+get_user_input()
+print_information_on_input_primality()
