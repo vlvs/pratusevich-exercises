4d7020e6528a998ee07d8752e80c7766cc6d9152
Vander <vlvsjr@gmail.com>
2021-03-17 15:26:22 -0300

    Solve 'List Ends' exercise

---
+++ 12_list_ends.py
@@ -0,0 +1,14 @@
+'''
+Write a program that takes a list of numbers and makes a new list of 
+only the first and last elements of the original list.
+'''
+
+import random
+
+def generate_sample_list():
+    return random.sample(range(100), random.randint(0, 30))
+
+def extract_list_ends(input_list):
+    return [input_list[0], input_list[len(input_list)-1]]
+
+print(extract_list_ends(generate_sample_list()))
